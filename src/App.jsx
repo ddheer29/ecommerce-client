8adf2885c80de9cc8229c8620f616bceb8cb1c40
Divyang Dheer <88249922+ddheer29@users.noreply.github.com>
2023-04-29 19:50:52 +0530

    context api setted up

--- src/App.jsx
+++ src/App.jsx
@@ -5,12 +5,16 @@ import About from './Pages/About'
 import Contact from './Pages/Contact'
 import Policy from './Pages/Policy'
 import PageNotFound from './Pages/PageNotFound'
+import Register from './Pages/Auth/Register'
+import Login from './Pages/Auth/Login'
 
 const App = () => {
     return (
         <>
             <Routes>
-                <Route path='/' element={<HomePage/>} />
+                <Route path='/' element={<HomePage />} />
+                <Route path='/register' element={<Register />} />
+                <Route path='/login' element={<Login />} />
                 <Route path='/about' element={<About />} />
                 <Route path='/contact' element={<Contact />} />
                 <Route path='/policy' element={<Policy />} />
--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,17 +1,34 @@
 import React from 'react'
 import Header from './Header'
 import Footer from './Footer'
+import { Helmet } from "react-helmet";
+import { Toaster } from 'react-hot-toast';
 
-const Layout = ({children}) => {
+const Layout = ({ children, title, description, keywords, author }) => {
     return (
         <div>
+            <Helmet>
+                <meta charSet="utf-8" />
+                <meta name="description" content={description} />
+                <meta name="keywords" content={keywords} />
+                <meta name="author" content={author} />
+                <title>{title}</title>
+            </Helmet>
             <Header />
-            <main style={{minHeight: "70vh"}}>
-            {children}
+            <main style={{ minHeight: "70vh" }}>
+                <Toaster />
+                {children}
             </main>
             <Footer />
         </div>
     )
+}
+
+Layout.defaultProps = {
+    title: 'Ecommerce App - Shop Now',
+    describe: 'mern stack project',
+    keywords: 'mern, react, node, express, mongo',
+    author: 'Divyang Dheer'
 }
 
 export default Layout
